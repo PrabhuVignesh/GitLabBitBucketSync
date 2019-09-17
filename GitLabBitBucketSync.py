c555601ddda4fa1aa583cd75be0abe9cd6f861ac
Prabhu Vignesh <prabhu.vignesh1990@gmail.com>
2019-09-17 14:01:12 +0000

    first commit

--- GitLabBitBucketSync.py
+++ GitLabBitBucketSync.py
@@ -3,6 +3,7 @@ from git import Repo
 import os
 import urllib.parse
 import errno, stat, shutil
+import datetime
 
 pwd = subprocess.check_output("pwd", shell=True, universal_newlines=True)
 #================================== From GitLab ===============================
@@ -18,7 +19,6 @@ else:
   os.system("rm -rf "+GitLab_Directory)
   os.mkdir(GitLab_Directory)
 base_location_gitlab = (str(pwd.rstrip("\n"))+"/"+GitLab_Directory)
-# git.Git(base_location_gitlab).clone("https://"+GitLab_Username+":"+GitLab_Password+"@"+GitLab_URL_Path_arr[1])
 Repo.clone_from("https://"+GitLab_Username+":"+GitLab_Password+"@"+GitLab_URL_Path_arr[1],base_location_gitlab,branch=GitLab_Branch)
 
 #================================== From BitBucket ===============================
@@ -34,28 +34,16 @@ else:
   os.system("rm -rf "+Bitbucket_Directory)
   os.mkdir(Bitbucket_Directory)
 base_location_bitbucket = (str(pwd.rstrip("\n"))+"/"+Bitbucket_Directory)
-# git.Git(base_location_bitbucket).clone("https://"+Bitbucket_Username+":"+Bitbucket_Password+"@"+Bitbucket_URL_Path_arr[1])
 Repo.clone_from("https://"+Bitbucket_Username+":"+Bitbucket_Password+"@"+Bitbucket_URL_Path_arr[1],base_location_bitbucket,branch=Bitbucket_Branch)
 
 #==================================== Directory Operations ================================
 os.system("rm -rf "+base_location_gitlab+"/.git/")
 os.system("cp -R "+base_location_bitbucket+"/.git "+base_location_gitlab)
-# os.system("cat "+base_location_gitlab+"/.git/config")
 #====================================
-COMMIT_MESSAGE = "Commiting from Python1" + os.getenv('BUILD_ID')
-# try:
-#     repo = Repo(base_location_gitlab+"/.git/")
-#     repo.git.add(update=True)
-#     repo.index.commit(COMMIT_MESSAGE)
-#     origin = repo.remote(name="https://"+Bitbucket_Username+":"+Bitbucket_Password+"@"+Bitbucket_URL_Path_arr[1])
-#     origin.push()
-# except:
-#     print('Some error occured while pushing the code')
+COMMIT_MESSAGE = "GitLab Sync from jenkins @" datetime.datetime.now().strftime("%A, %d. %B %Y %I:%M%p") + os.getenv('BUILD_ID')
 
 repo = Repo(base_location_gitlab+"/.git/")
-repo.git.checkout('origin/sync') #checkout to a branch linked to the other repo
+repo.git.checkout('origin/sync') 
 repo.git.add('--all')
 repo.index.commit(COMMIT_MESSAGE)
-repo.git.push('origin', 'HEAD:sync') # git push remote_to_push HEAD:master
-
-# os.system("cd "+base_location_gitlab+" && git checkout "+Bitbucket_Branch+" && git add --all "+" && git commit -m "+COMMIT_MESSAGE+ " && git push origin "+Bitbucket_Branch)
+repo.git.push('origin', 'HEAD:sync') 
